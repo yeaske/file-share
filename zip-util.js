400279398f46abb6951c62506e5e91b34480b0a6
Arun Suresh <yeaske@gmail.com>
2021-08-17 22:43:30 -0500

    Update zip-util.js

--- zip-util.js
+++ zip-util.js
@@ -1,7 +1,1 @@
-document.querySelector("#quadpay-section")
-widgetContainer.style.cursor = 'pointer';
-var modalOpen = document.querySelector('quadpay-widget').shadowRoot.querySelectorAll('.qp-open-modal-button')
-
-document.querySelector("#quadpay-section-s")
-widgetContainer.style.cursor = 'pointer';
-var modalOpen = document.querySelector('quadpay-widget').shadowRoot.querySelectorAll('.qp-open-modal-button')
+let modalOpen = document.querySelector('quadpay-widget').shadowRoot.querySelectorAll('.qp-open-modal-button');
