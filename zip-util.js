632b3323c6344be761b30736b4820fcb855cd92a
Arun Suresh <yeaske@gmail.com>
2021-08-17 22:24:51 -0500

    Create zip-util.js
    new file for zip

---
+++ zip-util.js
@@ -0,0 +1,7 @@
+document.querySelector("#quadpay-section")
+widgetContainer.style.cursor = 'pointer';
+var modalOpen = document.querySelector('quadpay-widget').shadowRoot.querySelectorAll('.qp-open-modal-button')
+
+document.querySelector("#quadpay-section-s")
+widgetContainer.style.cursor = 'pointer';
+var modalOpen = document.querySelector('quadpay-widget').shadowRoot.querySelectorAll('.qp-open-modal-button')
